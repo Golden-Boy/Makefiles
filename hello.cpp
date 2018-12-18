3be18ad542605a8edb35875f1706a25a3b913998
golden-boy <farbeyondaverage@gmail.com>
2018-12-18 11:28:30 -0800

    initial commit

---
+++ hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream.h>
+#include "example.h"
+
+void print_hello(){
+   cout << "Hello World!";
+}
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream.h>
+#include "example.h"
+
+int main(){
+    print_hello();
+    cout << endl;
+    cout << "Factorial output(3) " << factorial(3) << endl;
+    return 0;
+}
